0de2b44835ba654756d81e7ba60c12c427de45d6
daniela <daniela.salazar@tecsup.edu.pe>
2025-11-06 13:21:15 -0500

    Primer commit

---
+++ app/src/main/java/com/example/lab12_maps/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lab12_maps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MapScreen()
+        }
+    }
+}
